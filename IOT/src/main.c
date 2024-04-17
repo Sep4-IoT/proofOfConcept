710c1f7fa339b23607231ec723f82cee0aac6c88
JustAugucis <62718304+JustAugucis@users.noreply.github.com>
2024-04-17 13:07:09 +0200

    Reworked and first functioning IOT
    working connecting to wifi and posting a message

--- IOT/src/main.c
+++ IOT/src/main.c
@@ -1,80 +1,76 @@
-#include "includes.h"
-#include "periodic_task.h"
-#include "pc_comm.h"
-#include "display.h"
-#include "leds.h"
-#include "servo.h"
-#include "tone.h"
+
 #include "wifi.h"
+#include "display.h"
+#include "pc_comm.h"
+#include "includes.h"
+#include "servo.h"
+#include "buttons.h"
+#include "periodic_task.h"
 
 
-void func_a(){
-static uint16_t i = -10;
-display_int(i++);
+int window_state = 1;
+char message;
 
-pc_comm_send_string_blocking("!!!!!!!!!!!!!!!!!!!!!");
+void button_task(); 
+void decode(); // need to test of this works
+
+void set_window(){
+
+  if(window_state == 1)
+  {
+    servo(180);
+    uart_send_string_blocking(USART_0,"servo state 1 \n");
+  }
+  else {
+    servo(0);
+    uart_send_string_blocking(USART_0,"servo state 0 \n");
+  }
 }
 
-void func_b(){
-    static uint8_t state = 0;
-    if (state==0)
-    {
-        servo(50);
-        state = 1;
-    }
-    else {
-        servo(100);
-        state = 0;
-    }
-    
-
+void setup(){
+  pc_comm_init(9600,NULL);
+  wifi_init();
+  display_init();
+  buttons_init();
+  periodic_task_init_a(set_window,5000);
 }
 
-void func_c(){
-
+void decode(){ // need to test of this works
+  set_window();
 }
 
 
-int main(void)
-{
 
-    display_init();     
-    //tone_init();
-    //tone_play_starwars_tuned();
-    //leds_init();
-    pc_comm_init(9600, ((void *)0));
+int main(){
+  setup();
+  
+  display_int(1234);
 
-    //Join AP
-    
-    
-    wifi_init();
-
-    
-    char ssid[] = "augustinas";
-    char password[] = "asdfghjk";
-    wifi_command_join_AP(ssid, password);
-    wifi_command_AT();
-    
-    //periodic_task_init_a(func_a, 3300);
-    periodic_task_init_b(func_b, 1000);
-    //periodic_task_init_c(func_c, 1000);
-    
+  if (WIFI_OK==wifi_command_join_AP("augustinas","asdfghjk"))  
+    uart_send_string_blocking(USART_0,"Joined AP\n");
+  else 
+    uart_send_string_blocking(USART_0,"Failed to join AP\n");
 
 
-while (1) {
-    // Check Wi-Fi status, handle incoming data, etc.
-    // For example:
-    
 
-    // Perform other periodic tasks
-    // func_a();  // Uncomment if needed
-    // func_b();  // Uncomment if needed
+  if (WIFI_OK==wifi_command_create_TCP_connection("192.168.103.40", 23, decode, message))  // use this for callback and deciphering callback
+    uart_send_string_blocking(USART_0,"Joined TCP connection\n");
+    else
+    uart_send_string_blocking(USART_0,"Failed connecting tcp\n");
 
-    //_delay_ms(100); // Add some delay to prevent CPU overload
-}
-
-   
-
-
-    return 0;
+    _delay_ms(1000);
+  while (1)
+  {
+      window_state = 0;
+      wifi_command_TCP_transmit((uint8_t*)"Hi from Addell\n", 15);
+      _delay_ms(1000);
+      window_state = 1;
+      //servo(0);
+      wifi_command_TCP_transmit((uint8_t*)"Window Open\n", 12);
+      _delay_ms(1000);
+      wifi_command_TCP_transmit((uint8_t*)"Window Closed\n", 14);
+      //servo(1);
+      _delay_ms(1000);
+  }
+  
 }
