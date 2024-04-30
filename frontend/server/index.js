48e63008cca9b9213e376ddd3f580e843410af11
Jakub Kuka <jakubkuka07@gmail.com>
2024-04-30 10:33:59 +0200

    code clean up

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -1,52 +1,47 @@
+const jsonFilePath = path.join(__dirname, './greenhouse.json');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000; 
 const express = require('express');
 const fs = require('fs').promises;
-const bodyParser = require('body-parser');
 const cors = require('cors'); 
 const path = require('path');
-
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
-
-const jsonFilePath = path.join(__dirname, './greenhouse.json');
 
 app.get('/api/getGreenhouseData', async (req, res) => {
   try {
     const rawData = await fs.readFile(jsonFilePath);
     const jsonData = JSON.parse(rawData);
-
     const greenhouseData = {
-      temperature: jsonData.greenhouse.temperature,
-      humidity: jsonData.greenhouse.humidity,
+      id: jsonData.greenhouse.id,
+      name: jsonData.greenhouse.name,
+      window: jsonData.greenhouse.window,
     };
-
     res.status(200).json(greenhouseData);
-  } catch (error) {
+  } 
+  catch (error) {
     console.error('Error retrieving greenhouse data:', error);
     res.status(500).json({ message: 'Failed to retrieve greenhouse data' });
   }
 });
 
-
 app.put('/api/updateGreenhouse', async (req, res) => {
   try {
     const { window } = req.body;
-
     const rawData = await fs.readFile(jsonFilePath);
     const jsonData = JSON.parse(rawData);
-
     jsonData.greenhouse.window = window;
 
     await fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2));
-
     res.status(200).json({ message: 'Greenhouse window status updated successfully' });
-  } catch (error) {
+  } 
+  catch (error) {
     console.error('Error updating greenhouse window status:', error);
     res.status(500).json({ message: 'Failed to update greenhouse window status' });
   }
 });
-
-const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
